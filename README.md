0998f441f0e2418a30cf7ec5ab78ceaec72d8edc
Owen Adrian <akrivus@gmail.com>
2019-11-21 18:03:19 -0600

    Completed user interface and basic functionality.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # MobiMenu
 
-MobiMenu is an open-source portal for multimedia display units. It's use case is limitless, with applications ranging from menus, advertisements, presentations, and kiosks.
+MobiMenu is an open-source portal for multimedia display units. It's niche functionality is remarkably simple and designed for quick deployment and ease of use across multiple levels of technical aptitude.
 
-## Functions
+## Installation Notes
 
-* Display images to one or more monitors:
-  * Statically, for menus.
-  * Timed, for advertisements and presentations.
-* Define timed or conditional routines.
+* MobiMenu **only** operates for unix-based/like operating system due to its use of `fork`, a feature not supported on Windows. It is designed to operate on the Raspberry Pi 4 Model B.
+* Users must enter framebuffer devices (devices labeled `fb*` in `ls /dev/`) and their aspect ratios in the `display.csv` file for them to be acknowledged by MobiMenu.
+  * Columns are as follows: `framebuffer device`, `aspect ratio`, `human name`, `image name`
+  * Example row: `fb0,16:9,Default Monitor,default.jpg`
+* In addition to the framebuffer device, users are recommended to change the default password and session secret in their `.env` file so to prevent potentially unwanted changes.
---
+++ server.rb
@@ -0,0 +1,100 @@
+require 'dotenv/load'
+require 'sinatra'
+require 'csv'
+
+Displays = []
+class Display
+  attr_reader :path, :aspect_ratio, :name, :filename
+  def initialize(row)
+    @path, @aspect_ratio, @name = row[0..2]
+    image(row[3])
+  end
+  def image(filename)
+    @ratio = @aspect_ratio.split(':').map { |ration| ration.to_f }.inject(:/)
+    @filename = filename
+    if Process.respond_to? :fork
+      @proc.kill unless @proc.nil?
+      @proc = fork do
+        exec("fbi -d /dev/#{@path} ./public/#{filename}")
+      end unless filename.nil?
+    end
+  end
+  def width
+    return height * @ratio
+  end
+  def height
+    return 384.0
+  end
+  def from_params(name, filename)
+    image(filename) unless filename.nil?
+    @name = name
+    save
+  end
+  def to_a
+    [@path, @aspect_ratio, @name, @filename]
+  end
+  def save
+    CSV.open('display.csv', 'wb') do |csv|
+      DisplaySheet.each do |row|
+        row = to_a if row[0].eql? @path
+        csv << row
+      end
+    end
+  end
+  def self.find path
+    Displays.each do |display|
+      return display if display.path.eql? path
+    end
+  end
+end
+
+DisplaySheet = CSV.read('./display.csv')
+DisplaySheet.each do |row|
+  Displays << Display.new(row)
+end
+
+enable :sessions
+set :session_secret, ENV['SECRET']
+set :signed_in do |required|
+  condition do
+    redirect '/sign-in' unless session[:signed_in]
+  end if required
+end
+def message
+  return nil
+end
+
+get '/sign-out' do
+  erb(:sign_out)
+end
+get '/sign-in' do
+  erb(:sign_in)
+end
+post '/sign-in' do
+  unless params[:password].eql? ENV['PASSWORD']
+    erb(:sign_in, locals: { message: 'Incorrect password.' })
+  else
+    session[:signed_in] = true
+    redirect '/'
+  end
+end
+get '/', signed_in: true do
+  erb(:dashboard)
+end
+get '/display/:path', signed_in: true do
+  erb(:display, locals: { display: Display.find(params[:path]) })
+end
+post '/display/:path', signed_in: true do
+  display = Display.find(params[:path])
+  unless params[:image].nil?
+    display.from_params(params[:name], params[:image][:filename])
+    File.open("./public/images/#{params[:image][:filename]}", 'wb') do |file|
+      File.open(params[:image][:tempfile].path, 'rb') do |temp|
+        file.write(temp.read)
+      end
+    end
+  else
+    display.from_params(params[:name], nil)
+  end
+  redirect '/'
+end
