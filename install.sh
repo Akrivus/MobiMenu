696d68c82839cb4bded9db622c080b4e7eefcb12
Owen Adrian <akrivus@gmail.com>
2019-11-23 09:21:54 -0600

    Using CD to travel to repo directory.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt install nginx fim
 echo 'Configuring nginx...'
 sudo cp -f ./nginx.config /etc/nginx/sites-enabled/default
 echo 'Appending start up script to your bash profile.'
-echo 'bundle exec ruby ~/MobiMenu/server.rb' >> ~/.profile
+echo 'cd ~/MobiMenu/ && bundle exec ruby server.rb' >> ~/.profile
 echo 'Please configure your Pi to automatically log in.'
 sudo raspi-config
 echo 'Rebooting Pi...'
