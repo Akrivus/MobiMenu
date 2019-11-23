3dbb3a7dabb9042d309c6137e4b748bef860f185
Owen Adrian <akrivus@gmail.com>
2019-11-23 09:34:17 -0600

    Made installation script direct path.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt install nginx fim
 echo 'Configuring nginx...'
 sudo cp -f ./nginx.config /etc/nginx/sites-enabled/default
 echo 'Appending start up script to your bash profile.'
-echo 'cd ~/MobiMenu/ && bundle exec ruby server.rb' >> ~/.profile
+echo 'cd /home/pi/MobiMenu/ && bundle exec ruby server.rb' >> ~/.profile
 echo 'Please configure your Pi to automatically log in.'
 sudo raspi-config
 echo 'Rebooting Pi...'
