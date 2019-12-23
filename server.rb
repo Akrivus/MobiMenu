de51bee053330941f32176e9e0dec979729f1ac0
Owen Adrian <akrivus@gmail.com>
2019-12-23 16:52:06 -0600

    Removing 'production' from environment.

--- server.rb
+++ server.rb
@@ -69,7 +69,6 @@ end
 disable :logging
 enable :sessions
 set :session_secret, ENV['SECRET']
-set :environment, :production
 set :signed_in do |required|
   condition do
     redirect '/sign-in' unless session[:signed_in]
